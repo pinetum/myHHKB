c25907fa302c36c037094fccc6cf718930248b97
qtlintw <qtlintw@gmail.com>
2017-08-22 22:10:13 +0800

    First commit

---
+++ keyboard/hhkb/.dep/obj_hhkb_common_debug.o.d
@@ -0,0 +1,13 @@
+obj_hhkb/common/debug.o: ../../tmk_core/common/debug.c config.h \
+ ../../tmk_core/common/debug.h ../../tmk_core/common/print.h \
+ ../../tmk_core/common/util.h ../../tmk_core/common/avr/xprintf.h
+
+config.h:
+
+../../tmk_core/common/debug.h:
+
+../../tmk_core/common/print.h:
+
+../../tmk_core/common/util.h:
+
+../../tmk_core/common/avr/xprintf.h:
---
+++ keyboard/hhkb/.dep/obj_hhkb_common_util.o.d
@@ -0,0 +1,6 @@
+obj_hhkb/common/util.o: ../../tmk_core/common/util.c config.h \
+ ../../tmk_core/common/util.h
+
+config.h:
+
+../../tmk_core/common/util.h:
